7b8e887a7fa4e5cbfa186632a25f3a747d2982e2
VladimirKukharets <vladimir.kuharets@ukr.net>
2023-06-27 13:42:53 +0300

    Add new functionality

--- adress_book.py
+++ adress_book.py
@@ -1,7 +1,13 @@
 from collections import UserDict
+from datetime import datetime
+import re
 
 
 class AddressBook(UserDict):
+    def __init__(self):
+        super().__init__()
+        self.page_size = 1
+        self.count = 0
 
     def add_record(self, record):
         self.data[record.name.value] = record
@@ -18,12 +24,44 @@ class AddressBook(UserDict):
                 else:
                     print("I can't find this user or phone number")
 
+    def __next__(self):
+        items = list(self.data.items())
+        if self.count >= len(items):
+            raise StopIteration()
+        users = []
+        for i in range(self.count, min(self.count + self.page_size, len(items))):
+            key, val = items[i]
+            user_name = val.name.value
+            user_numbers = []
+            user_birthday = val.birthday
+
+            for phone in val.phones:
+                user_numbers.append(phone.value)
+
+            if val.birthday != None:
+                user_birthday = val.birthday.__dict__
+            else:
+                user_birthday = "None"
+
+            users.append(f"|{user_name:^40}|{', '.join(user_numbers):^40}|{user_birthday:^40}|")
+
+        self.count += self.page_size
+
+        for i in users:
+            print(i)
+
+        return users
+
+    def __iter__(self):
+        return self
+
 
 class Record:
 
-    def __init__(self, name, phone=None):
+    def __init__(self, name, phone=None, birthday=None):
         self.name = name
         self.phones = []
+        self.birthday = birthday
         if phone is not None:
             self.phones.append(phone)
 
@@ -40,6 +78,16 @@ class Record:
             if user_phone.value == old_phone.value:
                 self.phones[index] = new_phone
 
+    def days_to_birthday(self):
+        today = datetime.now()
+        birthday_in_this_year = datetime(year=today.year, month=self.birthday.value[1], day=self.birthday.value[0])
+        if birthday_in_this_year < today:
+            birthday_in_this_year = datetime(year=today.year + 1, month=int(self.birthday.value[1]),
+                                             day=int(self.birthday.value[0]))
+            return (birthday_in_this_year - today).days
+        else:
+            return (birthday_in_this_year - today).days
+
 
 class Field:
     def __init__(self, value):
@@ -51,4 +99,35 @@ class Name(Field):
 
 
 class Phone(Field):
-    pass
+
+    def __init__(self, value):
+        if self.validate_phone_number(value):
+            super().__init__(value)
+        else:
+            print("Invalid format or phone number")
+
+    @staticmethod
+    def validate_phone_number(phone_num):
+        pattern = r"^(?:\+?380|0)\d{9}$"
+        if re.match(pattern, phone_num):
+            return "validate"
+        else:
+            return "Incorrect phone number. Please try again."
+
+
+class Birthday(Field):
+    def __init__(self, value):
+        if self.validate_data(value):
+            super().__init__(value)
+        else:
+            print("Invalid format or phone number")
+
+    @staticmethod
+    def validate_data(birthday):
+        data_list = birthday.split("/")
+        if len(data_list) == 3:
+            day, month, year = map(int, data_list)
+            if 1 <= day <= 31 and 1 <= month <= 12:
+                return data_list
+        else:
+            print("Invalid data format. Must be in the format '31/12/1991'")
